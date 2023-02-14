ac1ff75e61b252741c049da9bf4d3983c57aab3a
Danish-Hussain-7 <93329106+Danish-Hussain-7@users.noreply.github.com>
2023-02-14 13:47:34 +0000

    danish

--- pages/about.vue
+++ pages/about.vue
@@ -24,26 +24,10 @@
 
 useHead({
     title: "3Squared",
-    link: [
-        {
-            rel: "stylesheet",
-            integrity: "sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=",
-            href: "https://unpkg.com/leaflet@1.9.3/dist/leaflet.css",
-            crossorigin: "",
+    
         },
-    ],
-    script: [
-        {
-            src: "https://unpkg.com/leaflet@1.9.3/dist/leaflet.js",
-            integrity: "sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=",
-            crossorigin: "",
-        },
-        {
-            src: "/leafletmap.js",
-            body: true,
-        },
-    ],
-});
+    
+);
 
 
 </script>
