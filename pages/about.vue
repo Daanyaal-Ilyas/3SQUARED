9769f1dfaf58dc04adbf0120a4074099dd8f0ca9
Mo-Rehman <119310300+Mo-Rehman@users.noreply.github.com>
2023-02-14 14:07:59 +0000

    about page

--- pages/about.vue
+++ pages/about.vue
@@ -10,7 +10,29 @@
         </nav>
     </header>
 
+<!-- this is the main content -->
+<main>
+      <h1>About Us</h1>
+      <p>We are 3Squared!
+Make.Change.Work.
+Digital innovation in rail is nothing new. But success has often been tactical, partial, temporary or not at all. So it’s not surprising that some stakeholders are difficult to bring on-side. That’s why forward-thinkers need more than just sharp technology. They need partners who are implementation experts, proven to win over sceptical stakeholders and deliver results in the real world of rail.
 
+And they need point solutions designed to grow into larger business answers.
+
+3Squared is the partner who has it all. The ethic, capacity, portfolio and roadmap to deliver success every step of the way.
+
+That’s why we say only 3Squared is truly in the business of client success, and not just software. That’s the 3Squared difference.
+
+Let’s Make.Change.Work.Together.</p>
+      
+      <p>If you have any questions or comments, please feel free to contact us:</p>
+      <ul>
+        <li>Phone: <a href="tel:+443331213333">+44 (0)333 121 3333</a></li>
+        <li>Email: <a href="mailto:hello@3squared.com">hello@3squared.com</a></li>
+      </ul>
+  
+      <p>Our software support desk is open from 09:00 to 17:00, Monday to Friday, excluding UK Bank Holidays.</p>
+    </main>
    
 
     <!-- this is the footer -->
@@ -80,6 +102,52 @@ button:hover::after {
     }
 
 /*test */
+
+
+/* Main content */
+main {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 2rem;
+    line-height: 1.6;
+  }
+
+  h1 {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+  }
+
+  address {
+    margin-bottom: 1rem;
+  }
+
+  ul {
+    margin-bottom: 1rem;
+  }
+
+  ul li {
+    margin-bottom: 0.5rem;
+  }
+
+  a {
+    color: #3282b8;
+    font-weight: bold;
+    text-decoration: none;
+    transition: color 0.2s ease-in-out;
+  }
+
+  a:hover {
+    color: #005d9a;
+  }
+
+ 
+
+  footer p {
+    margin: 0;
+    font-size: 0.8rem;
+  }
+
+
 
 /*Sidebar*/ 
 #sidebar {
