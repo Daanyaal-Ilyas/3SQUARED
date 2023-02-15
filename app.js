1eef81340509484abcaab7701b0f7a60deab6118
Daanyaal-Ilyas <93329164+Daanyaal-Ilyas@users.noreply.github.com>
2023-02-15 13:11:13 +0000

    c

---
+++ app.js
@@ -0,0 +1,79 @@
+import express, { json } from 'express';
+import fetch from 'node-fetch';
+import fs from 'fs';
+
+const app = express();
+
+app.use(express.static("./public"));
+
+app.use(json())
+
+const PORT = process.env.PORT || 3000;
+
+async function GetSchedule(){
+    const url = `https://traindata-stag-api.railsmart.io/api/trains/tiploc/CREWEMD/2023-02-14 00:00:00/2023-02-15 23:59:59`;
+    const options = {//WLSDEUT,LOWFRMT,WLSDRMT,CARLILE,MOSEUPY,STAFFRD,DONCIGB,THMSLGB,FLXSNGB
+        method: 'GET',
+        headers: {
+            'X-ApiVersion': "1",
+            'X-ApiKey': 'AA26F453-D34D-4EFC-9DC8-F63625B67F4A'
+        }
+    };
+    const apiResp = await fetch(url, options);
+    const json = await apiResp.json();
+
+    return json
+}
+
+app.get("/api/trainschedule", async function (req, resp) {
+
+    fs.readFile("schedule.json", async function (err, buffer) {
+        if (err)
+        {
+            console.log("Error reading File: " + err.message)
+        }
+        else
+        {
+            const json = JSON.parse(buffer.toString());
+
+            let trainSchedules = []
+
+	        for (const item of json) {
+                const { activationId, scheduleId } = item;
+                const url = `https://traindata-stag-api.railsmart.io/api/ifmtrains/schedule/${activationId}/${scheduleId}`;
+                const options = {
+                    method: 'GET',
+                    headers: {
+                        'X-ApiVersion': "1",
+                        'X-ApiKey': 'AA26F453-D34D-4EFC-9DC8-F63625B67F4A'
+                    }
+                };
+                const apiResp2 = await fetch(url, options);
+                const json = await apiResp2.json();
+            
+                trainSchedules.push(json)
+            }
+        
+            // Return the original response from the first API call
+            resp.send(trainSchedules)
+        }
+    })
+})
+
+app.get("/api/livetrain/:activationId/:scheduleId", async function (req, resp) {
+
+    const url = `https://traindata-stag-api.railsmart.io/api/ifmtrains/movement/${req.params.activationId}/${req.params.scheduleId}`
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-ApiVersion': "1",
+            'X-ApiKey': 'AA26F453-D34D-4EFC-9DC8-F63625B67F4A'
+        }
+    };
+    const apiResp = await fetch(url, options);
+    const json = await apiResp.json();
+
+    resp.json(json)
+})
+
+app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
---
+++ public/leafletmap.js
@@ -0,0 +1,13 @@
+var map = L.map('leafletmap').setView([55, 0], 7);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+L.tileLayer('https://tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openrailwaymap.org/">OpenRailWayMap</a>'
+}).addTo(map);
+
+
