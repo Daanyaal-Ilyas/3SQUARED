a98d1bdf0361a728c38f05c0bd908227dc06599f
rajadaanyaall <93329164+Daanyaal-Ilyas@users.noreply.github.com>
2023-03-13 09:49:17 +0000

    update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static("./public"));
 
 app.use(json())
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 let schedule = await GetSchedule()
 fs.writeFile("schedule.json", JSON.stringify(schedule), (function (err) {}))
