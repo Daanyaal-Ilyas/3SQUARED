777ed5f040b7c4ac6eecdc6150e96cc93fb15e78
Daanyaal-Ilyas <rajadaanyaall@gmail.com>
2023-03-22 02:08:30 +0000

    Update leafletmap.js

--- public/leafletmap.js
+++ public/leafletmap.js
@@ -1,7 +1,7 @@
 var map = L.map('leafletmap').setView([54, -0.5], 6);
 
 
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20,
 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(map);
